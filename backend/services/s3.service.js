dd161e6d7407fa226f1e067c7c6be1702c3322c2
Haziq <36412948+haziqachik@users.noreply.github.com>
2026-07-26 01:13:32 +0800

    Added S3 download and delete services

--- backend/services/s3.service.js
+++ backend/services/s3.service.js
@@ -1,6 +1,12 @@
-const { PutObjectCommand } = require("@aws-sdk/client-s3");
+const {
+  PutObjectCommand,
+  GetObjectCommand,
+  DeleteObjectCommand,
+} = require("@aws-sdk/client-s3");
+
 const s3 = require("../config/s3");
 
+// Upload a file to Amazon S3
 async function uploadFile(file, projectId) {
   const key = `projects/${projectId}/${Date.now()}-${file.originalname}`;
 
@@ -19,6 +25,28 @@ async function uploadFile(file, projectId) {
   };
 }
 
+// Download a file from Amazon S3
+async function downloadFile(key) {
+  const command = new GetObjectCommand({
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: key,
+  });
+
+  return await s3.send(command);
+}
+
+// Delete a file from Amazon S3
+async function deleteFile(key) {
+  const command = new DeleteObjectCommand({
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: key,
+  });
+
+  await s3.send(command);
+}
+
 module.exports = {
   uploadFile,
+  downloadFile,
+  deleteFile,
 };
